63c29dd3073f1ce947e1ccaf6bea12f84a2c4750
admin <admin@YD5-02>
2023-08-18 17:50:36 +0900

    ..

---
+++ H20230817/src/co/yedam/lombok/MainExe.java
@@ -0,0 +1,11 @@
+package co.yedam.lombok;
+
+public class MainExe {
+	public static void main(String[] args) {
+		Student s1 = new Student(101,"hwang",80);
+		s1.setStdNo(102);
+		s1.setStdName("minju");
+		s1.setScore(100);
+		
+	}
+}
--- Practice/src/co/yedam/prac/Practice2.java
+++ Practice/src/co/yedam/prac/Practice2.java
@@ -4,11 +4,34 @@ import java.util.*;
 public class Practice2 {
 	public static void main(String[] args)  {
 		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		str = str.toUpperCase();
 		
-		int n = sc.nextInt();
+		int count[] = new int[26];
+		for(int i=0; i<count.length; i++) {
+			count[i] = 0;
+		}
 		
+		for(int i=0; i<str.length(); i++) {
+			count[str.charAt(i)-65]++;
+		}
 		
+
+		int max=0;
+		int secMax=0;
+		for(int i=0; i<count.length; i++) {
+			if(count[max]<count[i]) max = i;
+		}
 		
+		char result=' ';
+		for(int i=0; i<count.length; i++) {
+			if(count[max]== count[i] && i!=max) {
+				result = '?';
+			}
+		}
+		if (result != '?') result = (char)(max+'A');
+		
+		System.out.println(result);
 	}
 
 }
